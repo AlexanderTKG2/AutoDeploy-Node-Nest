ffd2cb8289c17337a5839fe4c8a719028f5b6d4e
Alexander Peña <Alexander.Pena@theksquaregroup.com>
2025-08-08 17:39:23 -0600

    added intentional vulnerability to test autodeploy process

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import config from 'src/config';
+import * as _ from 'lodash';
 import { EmployeeService } from './employee.service';
 import type { CreateEmployeeData } from './models/employeeModels';
 
@@ -13,6 +14,8 @@ export class EmployeeController {
 
   @Post()
   create(@Body() employee: CreateEmployeeData) {
+    // Intentional vulnerability
+    _.merge(this.employeeService, employee);
     return this.employeeService.create(employee);
   }
 }
